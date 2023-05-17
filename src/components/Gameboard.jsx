fb9bdf5aeb4418d1b976ccf16f46239c94590fce
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2023-05-17 15:10:45 -0400

    enabled users to fetch quote and audio with the click of the playBtn

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,40 +1,78 @@
-import React from "react"
-import playBtn from '../images/playBtn.png'
-import Searchbar from "./Searchbar"
-import { useState, useEffect } from "react"
+import React from "react";
+import playBtn from '../images/playBtn.png';
+import pauseBtn from '../images/pause.png';
+import Searchbar from "./Searchbar";
+import { supabase } from "../client";
+import { useState, useRef } from "react";
 
+const Gameboard = () => {
+  const [quote, setQuote] = useState(null);
+  const [quotes, setQuotes] = useState([]);
+  const [num, setNum] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null);
 
+  const handlePlayClick = async () => {
+    setLoading(true);
+    try {
+      const { data } = await supabase.from('Quotes').select('*');
+      setQuotes(data.slice(0, 3));
+      setQuote(data[num]?.quote || "No quotes found");
+      handleAudioToggle();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-const Gameboard = ({maybe}) => { 
+  const handleNextQuote = () => {
+    setNum(num + 1);
+    setQuote(quotes[num + 1]?.quote || "No quotes found");
+  };
 
-    const [quote, setQuote] = useState("Quotes will appear here.")
+  const handleAudioToggle = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
 
-    useEffect(() => { 
-        async function GetQuotes(){ 
-            
-        }
-    })
-
-
-    return (
-        <div className="game-container">
- 
-            <div className="playBtn-container">
-
-            <img className="playBtn" src={playBtn} alt="" />
-
-
+  return (
+    <div className="game-container">
+      <div className="playBtn-container" onClick={handlePlayClick}>
+        <img
+          className="playBtn"
+          src={isPlaying ? pauseBtn : playBtn}
+          onClick={handleAudioToggle}
+          alt=""
+        />
+      </div>
+      <div className="quote-container">
+        {quote ? (
+          <>
+            <p className="quote">{quote}</p>
+            <div className="gameBtn-container">
+              <button onClick={handleNextQuote} disabled={num === 2}>
+                skip
+              </button>
             </div>
-            <div className="quote-container">
-                <p className="quote">"{quote}"</p>
-                <button>hint</button>
-                <span>Movie 1/3</span>
-            </div>
+            <span>Movie {num + 1}/3</span>
+          </>
+        ) : (
+          <p className="quote">Click the play button to get a quote</p>
+        )}
+        {quote && (
+          <audio ref={audioRef} src={quotes[num]?.audio}></audio>
+        )}
+      </div>
 
-        <Searchbar/>
-        </div>
-    )
-}
+      <Searchbar />
+    </div>
+  );
+};
 
-
-export default Gameboard
+export default Gameboard;
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -14,7 +14,7 @@ const Searchbar = () => {
           const res = await axios.get(
             `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${input}`
           );
-          setMovies(res.data.results.slice(0,10));
+          setMovies(res.data.results.slice(0,5));
         } catch (error) {
           console.error(error);
         }
